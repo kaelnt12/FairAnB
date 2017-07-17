b1a10a7c17446378ad2d9d1a679782bc2e3e6b1b
Sang Mai <maisang90dn@gmail.com>
2017-07-17 22:52:01 +0700

    Sang commit HomeShop and Shopping Cart

--- FairAnB/src/main/java/interfaces/AbstractPage.java
+++ FairAnB/src/main/java/interfaces/AbstractPage.java
@@ -9,5 +9,12 @@ public class AbstractPage {
 	public static final String LOGIN_FAILED_MESSAGE = "//div[contains(.,'Login Failed. Username or Password is incorrect.')]";
 	public static final String REGISTER_LINK = "//a[@class='registration-link']";
 	public static final String DYNAMIC_PAGE_HEADER = "//span[contains(.,'%s')]";
+	public static final String MINI_CART = "//div[@id='miniCart']//a[@id='open-cart']";
+	public static final String MINI_CART_COUNT = "//div[@id='miniCart']//span[@id='cartinfo']/span";
+	public static final String MINI_CART_PRODUCT_NAME = "//div[@id='shoppingcart']//td[contains(.,'%s')]";
+	public static final String MINI_CART_DELETE_PRODUCT_NAME = "//div[@id='shoppingcart']//td[contains(.,'%s')]/following::td/button[@class='close removeProductIcon']";
+	public static final String MINI_CART_PRODUCT_PRICE = "//div[@id='shoppingcart']//td[4)]";
+	public static final String MINI_CART_TOTAL_PRODUCT_PRICE = "//div[@id='shoppingcart']//font[@class='total-box-price']";
+	public static final String MINI_CART_CHECKOUT = "//div[@id='shoppingcart']//button[contains(.,'Checkout')]";
 
 }
